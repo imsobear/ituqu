ce9980244a71838f9c7b5420b33f54ba244b0536
tomayday <maydayjust@gmail.com>
2013-04-03 17:01:01 +0800

    修改devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,8 @@ class User < ActiveRecord::Base
   :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, 
-                  :remember_me, :login, :avatar, :avatar_cache, :city
+  attr_accessible :username, :email, :password, :remember_me, :login,
+                  :avatar, :avatar_cache, :city
 
   attr_accessor :login
 
