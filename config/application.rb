2d784b6d0986a78c297ea13782e9375843170fa5
tomayday <maydayjust@gmail.com>
2013-04-09 12:02:35 +0800

    fixed the error message

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Youji
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :cn
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
